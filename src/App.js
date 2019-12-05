cd2a1ace3d0b2506947afd171ad92e39567ed0fe
leodini <leodini@hotmail.com>
2019-12-05 20:37:49 -0300

    reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { createStore, combineReducers } from 'redux';
+import { Provider, useSelector, useDispatch } from 'react-redux'
+
+
+function counterReducer(state = { count: 0 }, action){
+  switch(action.type){
+    case 'INCREMENT_COUNT':
+      return {...state, count: state.count + 1}
+    case 'DECREMENT_COUNT':
+      return {...state, count: state.count -1}
+    default:
+      return state
+  }
+}
+
+function nameReducer(state = { name: '' }, action){
+  switch(action.type){
+    case 'UPDATE_NAME':
+      return { ...state, name: action.payload }
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  counterReducer,
+  nameReducer
+})
+
+
+const INITIAL_STATE = {}
+
+function Counter(){
+  const { count, name } = useSelector(state => ({ 
+  ...state.counterReducer,
+  ...state.nameReducer }))
+  const dispatch = useDispatch();
+  function increment(){
+    dispatch({
+      type: 'INCREMENT_COUNT'
+    })
+  }
+  
+  function decrement(){
+    dispatch({
+      type: 'DECREMENT_COUNT'
+    })
+  }
+  return(
+    <>
+    <h2>Counter: {count}</h2>
+    <h3>Name: {name}</h3>
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
+    </>
+  )
+}
+
+function Name() {
+  const dispatch = useDispatch()
+  function handleUpdateName(event){
+    dispatch({
+      type: 'UPDATE_NAME',
+      payload: event.target.value
+    })
+  }
+  return(
+    <div>
+      <input type="text" placeholder="input your name" 
+      onChange={handleUpdateName}/>
+    </div>
+  )
+}
+
+
+
+
+const store = createStore(rootReducer, INITIAL_STATE)
 
 function App() {
+
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Counter/>
+      <Name/>
+    </Provider>
   );
 }
 
